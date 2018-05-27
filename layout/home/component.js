8a4ddf1803ae7fa4a6e7a209739fa78d771b33a4
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-05-27 21:56:40 +0200

    Testimonials

--- layout/home/component.js
+++ layout/home/component.js
@@ -8,6 +8,7 @@ import Hero from 'layout/home/hero';
 import Services from 'layout/home/services';
 import Diferentiation from 'layout/home/diferentiation';
 import Team from 'layout/home/team';
+import Testimonials from 'layout/home/testimonials';
 
 class HomeComponent extends React.Component {
   render() {
@@ -20,6 +21,7 @@ class HomeComponent extends React.Component {
         <Services />
         <Diferentiation />
         <Team />
+        <Testimonials />
       </Layout>
     );
   }
---
+++ layout/home/testimonials/component.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Styles
+import './styles.scss';
+
+class TestimonialsComponent extends React.Component {
+  render() {
+    return (
+      <div className="c-testimonials">
+        <div className="testimonials-content">
+          <h4>Nuestras sonrisas</h4>
+          <h2>La mejor experiencia dental y con un resultado excelente</h2>
+          <p>María López: ortodoncia</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TestimonialsComponent;
