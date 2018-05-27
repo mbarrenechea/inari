2136cc2a580a916b4f78e220b0b0dea2f9d377ad
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-05-28 00:04:55 +0200

    Export config

--- layout/head/component.js
+++ layout/head/component.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import HeadNext from 'next/head';
 
+const stylesURL = process.env.NODE_ENV === 'production' ? '/inari' : '';
+
 class Head extends React.PureComponent {
   static propTypes = {
     title: PropTypes.string.isRequired, // Some pages don't have any title (think embed)
@@ -18,16 +20,7 @@ class Head extends React.PureComponent {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="author" content="Vizzuality" />
 
-        <link rel="icon" href="/static/favicon.ico" />
-
-        {/* Mobile Adress background */}
-        {/* Chrome, Firefox OS and Opera */}
-        <meta name="theme-color" content="#c32d7b" />
-        {/* Windows Phone */}
-        <meta name="msapplication-navbutton-color" content="#c32d7b" />
-        {/* iOS Safari */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="icon" href={`${stylesURL}/static/favicon.ico`} />
 
         <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
       </HeadNext>
