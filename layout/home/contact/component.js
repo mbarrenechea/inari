895d409b5f138e512b857b1c0e7d38fcb2663e5a
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-08-20 01:38:30 +0200

    Cantact: spinner and error managment

--- layout/home/contact/component.js
+++ layout/home/contact/component.js
@@ -1,34 +1,71 @@
 import React from 'react';
+import classnames from 'classnames';
 import { Form, Text, TextArea } from 'react-form';
 
 import 'whatwg-fetch';
+
+// Components
+import Spinner from 'components/ui/spinner';
 
 // Styles
 import './styles.scss';
 
 class Contact extends React.Component {
+  state = {
+    submitting: false,
+    submitted: false,
+    submittedError: false
+  }
+
   onSubmit = (values) => {
+    this.setState({ submitting: true });
     const {
       firstName,
       lastName,
       email,
-      tel
+      tel,
+      observations
     } = values;
 
-    fetch(`https://script.google.com/macros/s/AKfycbxSOt5Wzx8traptkU0elMVQSFiJC-rQcgwuezw9cN-Lpo6zSog/exec?firstName=${firstName}&lastName=${lastName}&email=${email}&tel=${tel}`, { method: 'GET' })
-      .then(() => { console.info('Success'); })
+    fetch(`https://script.google.com/macros/s/AKfycbxSOt5Wzx8traptkU0elMVQSFiJC-rQcgwuezw9cN-Lpo6zSog/exec?firstName=${firstName}&lastName=${lastName}&email=${email}&tel=${tel}&observations=${observations}`, { method: 'GET' })
+      .then(() => {
+        this.setState({
+          submitting: false,
+          submitted: true,
+          submittedError: false
+        });
+        console.info('Success');
+      })
       .catch((error) => {
+        this.setState({
+          submitting: false,
+          submitted: false,
+          submittedError: true
+        });
+
         console.error('request failed', error);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          this.setState({
+            submitting: false,
+            submitted: false,
+            submittedError: false
+          });
+        }, 5000);
       });
   }
 
   render() {
+    const { submitting, submitted, submittedError } = this.state;
+
     return (
       <div className="c-contact">
         <div className="wrapper">
           <div className="row center-xs">
             <div className="col-xs-12 col-sm-6">
               <h2>Contacto</h2>
+
               <Form onSubmit={this.onSubmit}>
                 {formApi => (
                   <form onSubmit={formApi.submitForm} id="contact-form">
@@ -57,8 +94,19 @@ class Contact extends React.Component {
                       <TextArea field="observations" id="observations" rows="8" />
                     </div>
 
-                    <button type="submit" className="c-form-btn -primary">
-                      Enviar
+                    <button
+                      type="submit"
+                      disabled={submitting || submitted || submittedError}
+                      className={classnames({
+                        'c-form-btn': true,
+                        '-success': submitted,
+                        '-error': submittedError
+                      })}
+                    >
+                      {submitting && <Spinner className="-tiny" />}
+                      {submitted && 'Gracias por contactar con nosotros.'}
+                      {submittedError && 'Parece que ha habido un error. Inténtelo de nuevo.'}
+                      {!submitted && !submittedError && 'Enviar'}
                     </button>
                   </form>
                 )}
